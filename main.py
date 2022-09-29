5e3ea1c6634de2b8aaa395c7ed6b5d28bb52ab1b
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-09-29 22:53:02 +0700

    fix: add image fallbacks

--- main.py
+++ main.py
@@ -6,19 +6,65 @@ csv_path = "export.csv"
 json_path = "export.json"
 
 
-def get_olid(title, author):
+def search_book(title, author):
     response = requests.get(
         f"http://openlibrary.org/search.json?title={title}&author={author}")
 
     if response.status_code != 200:
-        return ""
+        return {}
 
     results = response.json()
 
     if results["num_found"] == 0:
+        return {}
+
+    return results["docs"][0]
+
+
+def get_olid(book):
+    if "key" not in book:
         return ""
 
-    return results["docs"][0]["key"].split("/")[-1]
+    return book["key"].split("/")[-1]
+
+
+def get_cover_id(book):
+    if "cover_i" not in book:
+        return ""
+
+    return book["cover_i"]
+
+
+def enhance_row(row):
+    row["ISBN"] = row["ISBN"][2:-1]
+    row["ISBN13"] = row["ISBN13"][2:-1]
+    row["Open Library Link"] = ""
+    row["Cover Image"] = ""
+
+    if (row["ISBN"]):
+        row["Open Library Link"] = f"https://openlibrary.org/isbn/{row['ISBN']}"
+        row["Cover Image"] = f"https://covers.openlibrary.org/b/isbn/{row['ISBN']}-M.jpg?default=false"
+        return row
+
+    book = search_book(row["Title"], row["Author"])
+    olid = get_olid(book)
+    cover_id = get_cover_id(book)
+
+    if olid:
+      row["Open Library Link"] = f"https://openlibrary.org/works/{olid}"
+      row["Cover Image"] = f"https://covers.openlibrary.org/b/olid/{olid}-M.jpg?default=false"
+
+    if cover_id:
+      row["Cover Image"] = f"https://covers.openlibrary.org/b/id/{cover_id}-M.jpg?default=false"
+
+    return row
+
+
+def filter_row(row, filter_keys=[]):
+    if (len(filter_keys) == 0):
+        return row
+
+    return {key: row[key] for key in filter_keys}
 
 
 def csv_to_json(filepath, filter_keys=[]):
@@ -28,24 +74,9 @@ def csv_to_json(filepath, filter_keys=[]):
         reader = csv.DictReader(export_csv)
 
         for row in reader:
-            row["ISBN"] = row["ISBN"][2:-1]
-            row["ISBN13"] = row["ISBN13"][2:-1]
-
-            if (row["ISBN"] != ""):
-                row["Open Library Link"] = f"https://openlibrary.org/isbn/{row['ISBN']}"
-                row["Cover Image"] = f"https://covers.openlibrary.org/b/isbn/{row['ISBN']}-M.jpg?default=false"
-            else:
-                olid = get_olid(row["Title"], row["Author"])
-                row["Open Library Link"] = f"https://openlibrary.org/works/{olid}" if olid else ""
-                row["Cover Image"] = f"https://covers.openlibrary.org/b/olid/{olid}-M.jpg?default=false" if olid else ""
-
-            if (len(filter_keys)):
-                parsed.append({
-                    key: row[key] for key in filter_keys
-                })
-                continue
-
-            parsed.append(row)
+            enhanced = enhance_row(row)
+            filtered = filter_row(enhanced, filter_keys)
+            parsed.append(filtered)
 
     return parsed
 
