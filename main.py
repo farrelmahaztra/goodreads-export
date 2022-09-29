c9493ae28e9b6c1b8ab0e6633190b21161c21cda
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-09-29 19:41:20 +0700

    feat: add param to filter keys

--- main.py
+++ main.py
@@ -5,46 +5,57 @@ import requests
 csv_path = "export.csv"
 json_path = "export.json"
 
+
 def get_olid(title, author):
-  response = requests.get(f"http://openlibrary.org/search.json?title={title}&author={author}")
-  
-  if response.status_code != 200:
-    return ""
+    response = requests.get(
+        f"http://openlibrary.org/search.json?title={title}&author={author}")
 
-  results = response.json()
+    if response.status_code != 200:
+        return ""
 
-  if results["num_found"] == 0:
-    return ""
+    results = response.json()
 
-  return results["docs"][0]["key"].split("/")[-1]
-    
+    if results["num_found"] == 0:
+        return ""
 
-def csv_to_json(filepath):
-  parsed = []
+    return results["docs"][0]["key"].split("/")[-1]
 
-  with open(filepath) as export_csv:
-    reader = csv.DictReader(export_csv)
 
-    for row in reader:
-      row["ISBN"] = row["ISBN"][2:-1]
-      row["ISBN13"] = row["ISBN13"][2:-1]
+def csv_to_json(filepath, filter_keys=[]):
+    parsed = []
 
-      if (row["ISBN"] != ""):
-        row["Open Library Link"] = f"https://openlibrary.org/isbn/{row['ISBN']}"
-        row["Cover Image"] = f"https://covers.openlibrary.org/b/isbn/{row['ISBN']}-M.jpg" 
-      else:
-        olid = get_olid(row["Title"], row["Author"])
-        row["Open Library Link"] = f"https://openlibrary.org/works/{olid}"
-        row["Cover Image"] = f"https://covers.openlibrary.org/b/olid/{olid}-M.jpg"
-       
-      parsed.append(row)
+    with open(filepath) as export_csv:
+        reader = csv.DictReader(export_csv)
 
-  return parsed
+        for row in reader:
+            row["ISBN"] = row["ISBN"][2:-1]
+            row["ISBN13"] = row["ISBN13"][2:-1]
+
+            if (row["ISBN"] != ""):
+                row["Open Library Link"] = f"https://openlibrary.org/isbn/{row['ISBN']}"
+                row["Cover Image"] = f"https://covers.openlibrary.org/b/isbn/{row['ISBN']}-M.jpg"
+            else:
+                olid = get_olid(row["Title"], row["Author"])
+                row["Open Library Link"] = f"https://openlibrary.org/works/{olid}"
+                row["Cover Image"] = f"https://covers.openlibrary.org/b/olid/{olid}-M.jpg"
+
+            if (len(filter_keys)):
+                parsed.append({
+                    key: row[key] for key in filter_keys
+                })
+                continue
+
+            parsed.append(row)
+
+    return parsed
 
 
 def write_json(data, filepath):
-  with open(filepath, "w") as export_json:
-    export_json.write(json.dumps(data))
+    with open(filepath, "w") as export_json:
+        export_json.write(json.dumps(data))
 
-parsed = csv_to_json(csv_path)
+
+parsed = csv_to_json(
+    csv_path, ["Title", "Author", "Exclusive Shelf", "Cover Image", "Open Library Link"])
+
 write_json(parsed, json_path)
